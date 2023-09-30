8c33a5fdda73e6b49cb97d1f1fe34467e261e7f8
IG <iikim2511@gmail.com>
2023-09-30 18:50:11 -0400

    --

--- src/Sidebar/Sidebar.js
+++ src/Sidebar/Sidebar.js
@@ -5,7 +5,14 @@ import "./Sidebar.css";
 
 const Sidebar = ({ handleChange }) => {
 
-  // 🦄console-handleChange 찍어보면 setSelectedCategory 가 나옴... 👉여기에서 set~을 해서 props 자식 컴포넌트인데도 불구하고, 상위컴포넌트에 데이터 전달할 수있는 것임
+
+
+  /* 🦄
+  console-handleChange 찍어보면 setSelectedCategory 가 나옴... 
+  👉여기에서 set~을 해서 props 자식 컴포넌트인데도 불구하고, 상위컴포넌트에 데이터 전달할 수있는 것임
+  
+  input value onChange한때 handleChange function 실행
+  */
   
   console.log(handleChange)
 
